f2b6499dd9e202ebe60b5d620721104999ee4160
parshanth <Parshanth2005@gmail.com>
2025-06-21 20:09:10 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,33 @@ connectDB();
 
 console.log('Connected to MongoDB');
 
-app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+  res.render('pages/login');
+});
+
+app.get('/home', (req, res) => {
+  res.render('pages/home');
+});
+
 app.use('/api/students', studentRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Dynamic certificate verification page
 app.get('/verify/:id', (req, res) => {
